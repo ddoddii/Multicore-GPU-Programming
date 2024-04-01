2346df202eb42630767b4b9b81351b5673cda62b
ddoddii <uhmturks@yonsei.ac.kr>
2024-04-01 04:49:06 +0000

    HW1

---
+++ HW1/README.md
@@ -0,0 +1,35 @@
+# Homework 1
+
+## Summary
+
+We count from 0. That's why this homework is #1.
+You should implement a simple filter on 1D array for parallelization
+
+Go to `filter.cc` and implement `TODO`
+
+```
+$ make remote
+$ cat out.txt
+init took 0.878147 sec
+serial 1D filter took 1.33711 sec
+parallel 1D filter took 0.433189 sec
+PASS
+```
+
+## Functions
+
+```
+# Run locally
+make
+make run
+# Compile
+make filter
+# Clean
+make clean
+```
+
+## References
+
+- [std::thread - cplusplus.com](http://www.cplusplus.com/reference/thread/thread/)
+- [OpenMP 4.5 C/C++ Syntax Guide](https://www.openmp.org/wp-content/uploads/OpenMP-4.5-1115-CPP-web.pdf)
+- [CMake Cheatsheet](https://github.com/mortennobel/CMake-Cheatsheet/blob/master/CMake_Cheatsheet.pdf)
---
+++ HW1/filter.cc
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+// #include <cstdio>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+
+int main(int argc, char** argv) 
+{
+
+  if(argc < 2) std::cout<<"Usage : ./filter num_items"<<std::endl;
+  int N = atoi(argv[1]);
+  int NT=32; //Default value. change it as you like.
+
+
+  //0. Initialize
+
+  const int FILTER_SIZE=5;
+  const float k[FILTER_SIZE] = {0.125, 0.25, 0.25, 0.25, 0.125};
+  float *array_in = new float[N];
+  float *array_out_serial = new float[N];
+  float *array_out_parallel = new float[N];
+  {
+    std::chrono::duration<float> diff;
+    auto start = std::chrono::steady_clock::now();
+    for(int i=0;i<N;i++) {
+      array_in[i] = i;
+    }
+    auto end = std::chrono::steady_clock::now();
+    diff = end-start;
+    std::cout<<"init took "<<diff.count()<<" sec"<<std::endl;
+  }
+
+  {
+    //1. Serial
+    std::chrono::duration<float> diff;
+    auto start = std::chrono::steady_clock::now();
+    for(int i=0;i<N-4;i++) {
+      for(int j=0;j<FILTER_SIZE;j++) {
+        array_out_serial[i] += array_in[i+j] * k[j];
+      }
+    }
+    auto end = std::chrono::steady_clock::now();
+    diff = end-start;
+    std::cout<<"serial 1D filter took "<<diff.count()<<" sec"<<std::endl;
+  }
+
+  {
+    //2. parallel 1D filter
+    std::chrono::duration<float> diff;
+    auto start = std::chrono::steady_clock::now();
+    /* TODO: put your own parallelized 1D filter here */
+    /****************/
+    std::vector<std::thread> threads;
+    int chunkSize = (N-4)/NT;
+
+    auto parallelFilter = [&](int start, int end) {
+            for (int i = start; i < end; i++) {
+                float sum = 0.0f;
+                for (int j = 0; j < FILTER_SIZE; j++) {
+                    sum += array_in[i + j] * k[j];
+                }
+                array_out_parallel[i] = sum;
+            }
+        };
+
+
+    for(int t=0; t<NT; t++){
+	    int startIdx = t*chunkSize;
+	    int endIdx = (t==NT-1) ? N -4 : (t+1) * chunkSize;
+	    threads.emplace_back(parallelFilter, startIdx, endIdx);
+    }
+
+    for (auto& thread : threads) {
+	    thread.join();
+    }
+
+
+    /****************/
+    /* TODO: put your own parallelized 1D filter here */
+    auto end = std::chrono::steady_clock::now();
+    diff = end-start;
+    std::cout<<"parallel 1D filter took "<<diff.count()<<" sec"<<std::endl;
+
+
+
+    int error_counts=0;
+    const float epsilon = 0.01;
+    for(int i=0;i<N;i++) {
+      float err= std::abs(array_out_serial[i] - array_out_parallel[i]);
+      if(err > epsilon) {
+        error_counts++;
+        if(error_counts < 5) {
+          std::cout<<"ERROR at "<<i<<": Serial["<<i<<"] = "<<array_out_serial[i]<<" Parallel["<<i<<"] = "<<array_out_parallel[i]<<std::endl;
+          std::cout<<"err: "<<err<<std::endl;
+        }
+      }
+    }
+
+
+    if(error_counts==0) {
+      std::cout<<"PASS"<<std::endl;
+    } else {
+      std::cout<<"There are "<<error_counts<<" errors"<<std::endl;
+    }
+
+  }
+  return 0;
+}
