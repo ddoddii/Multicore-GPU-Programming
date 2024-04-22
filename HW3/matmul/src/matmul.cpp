1e65e68394c768a6847f94c3edd616127b71f547
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-04-23 01:37:17 +0900

    HW3 init

---
+++ HW3/matmul/src/matmul.cpp
@@ -0,0 +1,15 @@
+#include "matmul.h"
+
+void matmul_ref(const int* const matrixA, const int* const matrixB,
+                int* const matrixC, const int n, const int m) {
+  // You can assume matrixC is initialized with zero
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      for (int k = 0; k < m; k++)
+        matrixC[i * n + j] += matrixA[i * m + k] * matrixB[k * n + j];
+}
+
+void matmul_optimized(const int* const matrixA, const int* const matrixB,
+                      int* const matrixC, const int n, const int m) {
+  // TODO: Implement your code
+}
