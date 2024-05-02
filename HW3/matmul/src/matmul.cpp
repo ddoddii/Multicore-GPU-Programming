a49ac7aca6794d58fbc17f61ae870f303edfe0c0
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-05-03 02:03:06 +0900

    feat : matmul optimization

--- HW3/matmul/src/matmul.cpp
+++ HW3/matmul/src/matmul.cpp
@@ -1,15 +1,45 @@
 #include "matmul.h"
+#include <omp.h>
+#include <pthread.h>
+#include <vector>
 
-void matmul_ref(const int* const matrixA, const int* const matrixB,
-                int* const matrixC, const int n, const int m) {
-  // You can assume matrixC is initialized with zero
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      for (int k = 0; k < m; k++)
-        matrixC[i * n + j] += matrixA[i * m + k] * matrixB[k * n + j];
+#define THREADS 32
+
+void matmul_ref(const int *const matrixA, const int *const matrixB, int *const matrixC, const int n, const int m)
+{
+    // You can assume matrixC is initialized with zero
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k < m; k++)
+                matrixC[i * n + j] += matrixA[i * m + k] * matrixB[k * n + j];
 }
 
-void matmul_optimized(const int* const matrixA, const int* const matrixB,
-                      int* const matrixC, const int n, const int m) {
-  // TODO: Implement your code
+void transpose(const int *const matrixB, int *const matrixB_trans, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < m; k++)
+        {
+            matrixB_trans[k * n + i] = matrixB[i * m + k];
+        }
+    }
+}
+
+void matmul_optimized(const int *const matrixA, const int *const matrixB, int *const matrixC, const int n, const int m)
+{
+    std::vector<int> B_trans(m * n);
+    transpose(matrixB, B_trans.data(), n, m);
+#pragma omp parallel for shared(matrixA, matrixB, matrixC) schedule(static) num_threads(THREADS)
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < m; k++)
+            {
+                sum += matrixA[i * m + k] * B_trans[j * m + k];
+            }
+            matrixC[i * n + j] = sum;
+        }
+    }
 }
